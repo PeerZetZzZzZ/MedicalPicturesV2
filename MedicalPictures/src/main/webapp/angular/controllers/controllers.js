785e66fd35d69b4b35713fa1b60108233c579c70
Przemyslaw Thomann <peerzet3@gmail.com>
2014-11-01 23:58:11 +0100

    Changed some css and added UserSettings ver.1

--- MedicalPictures/src/main/webapp/angular/controllers/controllers.js
+++ MedicalPictures/src/main/webapp/angular/controllers/controllers.js
@@ -1,34 +1,25 @@
  'use strict'
-
- var MedicalPictures = angular.module('MedicalPictures',['pascalprecht.translate']);
-MedicalPictures.GLOBAL_APP_NAME="MedicalPictures ver.1.0";
- MedicalPictures.controller('LoginController',function($scope){
-   $scope.name='Kon';
-   $scope.minPasswordLength="3";
-   $scope.maxPasswordLength="30";
-   $scope.prefered='en;'
+/* Contains global settings accessible for all controllers */
+ var MedicalPictures = angular.module('MedicalPictures',['pascalprecht.translate']).constant('MedicalPicturesGlobal',{
+   GLOBAL_APP_NAME:"MedicalPictures ver.1.0",
+   MIN_PASSWORD_LENGTH:"3",
+   MAX_PASSWORD_LENGTH:"30",
  });
- MedicalPictures.config(function($translateProvider){
-  $translateProvider.translations('en',{
-    'LOGIN_VIEW_HEAD_NAME':'MedicalPictures Login View',
-    'PASSWORD':'Password',
-    'LOG_IN':'Login',
-    'PROVIDE_CREDENTIALS':'Provide your credentials',
-    'INCORRECT_EMAIL':'Given username (e-mail address) is incorrect',
-    'PASSWORD_TOO_SHORT':'Password is too short!',
-    'PASSWORD_TOO_LONG':'Password is too long!',
 
-  });
-  $translateProvider.translations('pl',{
-    'LOGIN_VIEW_HEAD_NAME':'MedicalPictures Panel Logowania',
-    'USERNAME':'Użytkownik',
-    'LOG_IN':'Zaloguj',
-    'PASSWORD':'Hasło',
-    'PROVIDE_CREDENTIALS':'Podaj swój login oraz hasło',
-    'INCORRECT_EMAIL':'Podana nazwa użytkownika (adres e-mail) jest niepoprawny',
-    'PASSWORD_TOO_SHORT':'Hasło jest za krótkie!',
-    'PASSWORD_TOO_LONG':'Hasło jest za długie!',
-
-  });
-  $translateProvider.preferredLanguage('pl');
+ /* LoginView Controller */
+ MedicalPictures.controller('LoginController',function($scope, MedicalPicturesGlobal){
+   $scope.appName=MedicalPicturesGlobal.GLOBAL_APP_NAME;
+   $scope.minPasswordLength=MedicalPicturesGlobal.MIN_PASSWORD_LENGTH;
+   $scope.maxPasswordLength=MedicalPicturesGlobal.MAX_PASSWORD_LENGTH;
  });
+
+ /* AdminView Controller */
+ MedicalPictures.controller('AdminViewController',function($scope, MedicalPicturesGlobal){
+   $scope.appName=MedicalPicturesGlobal.GLOBAL_APP_NAME;
+ });
+
+/* UserSettings Controller */
+MedicalPictures.controller('UserSettingsController',function($scope,MedicalPicturesGlobal){
+  $scope.appName=MedicalPicturesGlobal.GLOBAL_APP_NAME;
+  
+})
