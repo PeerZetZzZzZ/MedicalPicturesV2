54ac90f7666decffc94a8a20a3cdcad03ca72fc1
Peer <peerzet3@gmail.com>
2014-11-06 09:27:07 +0100

    Przeniesione do web -inf i dodany request dispatcher w LoginServlet.java

--- MedicalPictures/src/main/java/peer/medicalpictures/controller/LoginServlet.java
+++ MedicalPictures/src/main/java/peer/medicalpictures/controller/LoginServlet.java
@@ -20,49 +20,56 @@ import org.json.JSONObject;
  */
 public class LoginServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+		for (Cookie c : cookies) {
+			System.out.println("Cookies z get" + c.getValue());
+		}
+		request.getRequestDispatcher("/WEB-INF/patientView.html").forward(request, response);
+	}
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("!");
-        StringBuffer jb = new StringBuffer();
-        String line = null;
-        try {
-            BufferedReader reader = request.getReader();
-            while ((line = reader.readLine()) != null) {
-                jb.append(line);
-            }
-        } catch (Exception e) { /*report an error*/ }
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("!");
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				jb.append(line);
+			}
+		} catch (Exception e) { /*report an error*/ }
 
-        JSONObject jsonObject = new JSONObject(jb.toString());
-        System.out.println(jb);
-        Cookie cookie = new Cookie("ciastko", "mojeNoweCiastko");
-        response.addCookie(cookie);
-        Cookie[] cookies = request.getCookies();
-        for (Cookie c : cookies) {
-            System.out.println("Cookies" + c.getValue());
-        }
-    }
+		JSONObject jsonObject = new JSONObject(jb.toString());
+		System.out.println(jb);
+		Cookie cookie = new Cookie("ciastko", "mojeNoweCiastko");
+		response.addCookie(cookie);
+		Cookie[] cookies = request.getCookies();
+		for (Cookie c : cookies) {
+			System.out.println("Cookies" + c.getValue());
+		}
+		request.getRequestDispatcher("/MedicalPictures/views/LoginView.html").forward(request, response);
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 }
