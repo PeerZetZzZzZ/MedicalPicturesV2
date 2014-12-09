2c31f60663dab5168f90a37d55fe73db9032a7b5
Peer <peerzet3@gmail.com>
2014-12-09 08:50:02 +0100

    Dodane troche z braintracka

---
+++ MedicalPictures/src/main/java/medicalpictures/model/common/MedicalLogger.java
@@ -0,0 +1,118 @@
+package medicalpictures.model.common;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+/**
+ * Medical logger used to log every important event in application. Log is made in standard output ( for example Glassfish output ) and log file (log file location for glassfish:
+ * glassfishInstallation/glassfish/domains/domain1/config/ ). Log file name is today's date in such form: YYYY-MM-DD.txt
+ *
+ * @author Przemysław Thomann
+ */
+@Singleton
+public class MedicalLogger {
+
+	private static final Logger LOG = Logger.getLogger(MedicalLogger.class.getName());
+	private File logFile;
+	private boolean logFileCreated = false;
+	private PrintWriter logOutput;
+
+	@PostConstruct
+	public void initLogFile() {
+		LocalDate now = new LocalDate();
+		String logFileName = "../" + now.toString() + ".txt";
+		logFile = new File(logFileName);
+		try {
+			if (!logFile.exists()) {
+				logFile.createNewFile();
+				LOG.info("Created log file: " + logFileName + ". Log file path: " + logFile.getAbsolutePath());
+				logOutput = new PrintWriter(new FileWriter(logFile, true));
+				LOG.info("Created writer for log file: " + logFileName);
+				logFileCreated = true;//successfully craeted log file
+			} else {
+				logOutput = new PrintWriter(new FileWriter(logFile, true));
+				logFileCreated = true;
+			}
+		} catch (IOException ex) {
+			LOG.info("ERROR! Couldn't create log file: " + logFileName);
+			logFileCreated = false;//creation failed
+			Logger.getLogger(MedicalLogger.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void logInfo(String message, Class infoClass) {
+		if (logFileCreated) {
+			DateTime now = new DateTime();
+			String infoMessage = "[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[INFO] " + message;
+			logOutput.println(infoMessage);
+			logOutput.flush();
+			LOG.info(infoMessage);
+		}
+	}
+
+	public void logError(String message, Class infoClass, Throwable ex) {
+		if (logFileCreated) {
+			DateTime now = new DateTime();
+			String errorMessage = "[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[ERROR] " + message;
+			logOutput.println(errorMessage);
+			logOutput.println("[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[STACKTRACE] ");
+			logOutput.println("");
+			logOutput.flush();
+			for (StackTraceElement element : ex.getStackTrace()) {
+				logOutput.println("   " + element.toString());
+			}
+			logOutput.flush();
+			logOutput.println("");
+			logOutput.flush();
+			LOG.info(errorMessage);
+			Logger.getLogger(infoClass.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void logWarning(String message, Class infoClass, Throwable ex) {
+		if (logFileCreated) {
+			DateTime now = new DateTime();
+			String warningMessage = "[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[WARNING] " + message;
+			logOutput.println(warningMessage);
+			logOutput.println("[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[STACKTRACE] ");
+			logOutput.println("");
+			logOutput.flush();
+			for (StackTraceElement element : ex.getStackTrace()) {
+				logOutput.println("   " + element.toString());
+			}
+			logOutput.flush();
+			logOutput.println("");
+			logOutput.flush();
+			LOG.info(warningMessage);
+		}
+	}
+
+	public void logWarning(String message, Class infoClass) {
+		if (logFileCreated) {
+			DateTime now = new DateTime();
+			String warningMessage = "[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[WARNING] " + message;
+			logOutput.println(warningMessage);
+			logOutput.println("[" + now.toString() + "]" + "[" + infoClass.getName() + "]" + "[STACKTRACE] ");
+			logOutput.println("");
+			logOutput.flush();
+			LOG.info(warningMessage);
+		}
+	}
+
+	@PreDestroy
+	public void closeAll() {
+		if (logFileCreated) {
+			logOutput.flush();
+			logOutput.close();
+		}
+	}
+}
---
+++ MedicalPictures/src/main/java/medicalpictures/model/common/ResultCodes.java
@@ -0,0 +1,66 @@
+package medicalpictures.model.common;
+
+/**
+ * Contains informations about result codes returned to the client.
+ *
+ * @author Przemysław Thomann
+ */
+public class ResultCodes {
+
+	public static ResultCodes INSTANCE = new ResultCodes();
+
+	private ResultCodes() {
+
+	}
+
+	/**
+	 * When operation finished successfully.
+	 */
+	public static int OPERATION_SUCCEED = 0;
+	/**
+	 * When internal server problem occurred
+	 */
+	public static int INTERNAL_SERVER_ERROR = -2;
+	/**
+	 * When user is not authorized to access specified content.
+	 */
+	public static int USER_UNAOTHRIZED = -1;
+	/**
+	 * When it was error while parsing input JSON.
+	 */
+	public static int INPUT_JSON_PARSE_ERROR = -3;
+	/**
+	 * When it was error while parsing input JSON.
+	 */
+	public static int USER_IS_NOT_LOGGED = -4;
+	/**
+	 * When user can't be registred, because is already registred.
+	 */
+	public static int OBJECT_ALREADY_EXISTS = -1001;
+	/**
+	 * When user is already logged.
+	 */
+	public static int USER_ALREADY_LOGGED = 3;//it's never sent to client that's why it's this value
+	/**
+	 * When user authentication failed while login.
+	 */
+	public static int USER_AUTHENTICATION_FAILED = -1002;
+	/**
+	 * When user authentication failed while login.
+	 */
+	public static int USER_LOGOUT_FAILED = -1004;
+
+	/**
+	 * When object doesn't exist for example GROUP ( except USER, it has own code )
+	 */
+	public static int OBJECT_DOESNT_EXIST = -1020;
+	/**
+	 * When user doesn't exist.
+	 */
+	public static int USER_DOESNT_EXIST = -1021;
+
+	/**
+	 * When operation couldn't finish successfully, because of error connected with Braintrack operation logic.
+	 */
+	public static int OPERATION_NOT_SUCCEED = -5;
+}
